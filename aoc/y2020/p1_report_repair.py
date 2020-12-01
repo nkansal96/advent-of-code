0b3975e93b9694555adda364c58729c01005b51d
Nikhil Kansal <nikhil@stripe.com>
2020-12-01 13:51:23 -0800

    add 2020 p1

---
+++ aoc/y2020/p1_report_repair.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+from aoc.common import input
+from aoc.common import solution
+
+
+@solution.part_one(year=2020, problem=1)
+def part_one(data: input.Tokenized["\n"]) -> int:
+    items = data.each_token_as(int)
+    counts = Counter(items)
+
+    for i in items:
+        j = 2020 - i
+        if j in counts:
+            if j == i and counts.get(j, 0) < 2:
+                continue
+            return i * j
+
+
+@solution.part_two(year=2020, problem=1)
+def part_two(data: input.Tokenized["\n"]) -> int:
+    items = list(data.each_token_as(int))
+    counts = Counter(items)
+
+    for i in range(0, len(items) - 1):
+        for j in range(i + 1, len(items)):
+            third = 2020 - (items[i] + items[j])
+            if third in counts:
+                if third == (items[i] + items[j]) and counts.get(third, 0) < 2:
+                    continue
+                return third * items[i] * items[j]
