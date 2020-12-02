2b90b3c4f43797f31674546bc750f8e6c79464ec
Nikhil Kansal <nkansal96@gmail.com>
2020-12-01 21:11:30 -0800

    add 2020 p2

---
+++ aoc/y2020/p2_password_philosophy.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from typing import Tuple
+
+import itertools
+import re
+
+from aoc.common import input
+from aoc.common import solution
+
+
+def parse_password(line: str) -> Tuple[int, int, str, str]:
+    m = re.match(r"(\d+)-(\d+) (\w+): (\w+)", line)
+    return int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
+
+
+@solution.part_one(year=2020, problem=2)
+def part_one(data: input.Tokenized["\n"]) -> int:
+    def is_password_valid(lower: int, upper: int, ch: str, pw: str):
+        return lower <= Counter(pw).get(ch, 0) <= upper
+
+    passwords = data.each_token_as(parse_password)
+    return sum(itertools.starmap(is_password_valid, passwords))
+
+
+@solution.part_two(year=2020, problem=2)
+def part_two(data: input.Tokenized["\n"]) -> int:
+    def is_password_valid(lower: int, upper: int, ch: str, pw: str):
+        return (pw[lower - 1] == ch) ^ (pw[upper - 1] == ch)
+
+    passwords = data.each_token_as(parse_password)
+    return sum(itertools.starmap(is_password_valid, passwords))
